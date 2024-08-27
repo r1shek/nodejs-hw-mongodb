d56d071fe62618c26e8054e307263cf700909665
r1shek <rostic523@gmail.com>
2024-08-27 18:43:11 +0200

    part1

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = 3000;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+};
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
