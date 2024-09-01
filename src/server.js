7e3d05c02a5bd0f6708bf7ade4cc2a7bc8b51d6b
r1shek <rostic523@gmail.com>
2024-09-01 10:51:42 +0200

    fix status 200
    fix status 200

--- src/server.js
+++ src/server.js
@@ -23,9 +23,11 @@ export const startServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
-    res
-      .status(200)
-      .json({ message: 'Successfully found contacts!', data: contacts });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
   app.get('/contacts/:contactById', async (req, res, next) => {
@@ -40,6 +42,7 @@ export const startServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contact with id {contactId}!',
       data: contact,
     });
