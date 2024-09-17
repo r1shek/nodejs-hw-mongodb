4ebb638a4fc49fb4bf1ed4eee453e0684911848f
r1shek <rostic523@gmail.com>
2024-09-17 09:34:28 +0200

    fix err  response

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,12 +7,17 @@ export const createContactSchema = Joi.object({
     'string.max': 'Username should have at most 30 characters',
     'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.number().integer().min(8).max(14).required().messages({
-    'number.base': 'Phone number should be a string',
-    'number.min': 'Phone number should have at least 8 characters',
-    'number.max': 'Phone number should have at most 14 characters',
-    'any.required': 'Phone number is required',
-  }),
+  phoneNumber: Joi.number()
+    .integer()
+    .min(10000000)
+    .max(99999999999999)
+    .required()
+    .messages({
+      'number.base': 'Phone number should be a string',
+      'number.min': 'Phone number should have at least 8 characters',
+      'number.max': 'Phone number should have at most 14 characters',
+      'any.required': 'Phone number is required',
+    }),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
@@ -20,7 +25,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number().integer().min(8).max(14),
+  phoneNumber: Joi.number().integer().min(10000000).max(99999999999999),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
